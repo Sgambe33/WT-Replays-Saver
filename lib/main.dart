f8397769bb736fb5cd7f3c94e7b44e6b34fc4cc8
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-04-20 15:44:27 +0200

    Switched to dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'dart:convert' show ascii, base64, json, jsonDecode;
+import 'dart:convert' show ascii, base64, json;
 
 import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:wtstatsgagaaapp/battles_history_page.dart';
-import 'package:wtstatsgagaaapp/login_page.dart';
-import 'package:wtstatsgagaaapp/page_selector.dart';
-import 'package:wtstatsgagaaapp/replays_page.dart';
-import 'package:wtstatsgagaaapp/stats_page.dart';
+import 'package:wtreplaysaver/pages/login_page.dart';
+import 'package:wtreplaysaver/pages/page_selector.dart';
 
 const storage = FlutterSecureStorage();
 
 void main() {
   storage.deleteAll();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   Future<String> get jwtOrEmpty async {
     var jwt = await storage.read(key: "jwt");
     if (jwt == null || JwtDecoder.isExpired(jwt)) {
@@ -31,9 +29,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Authentication Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark(), // Use dark theme
       home: FutureBuilder(
           future: jwtOrEmpty,
           builder: (context, snapshot) {
@@ -45,8 +41,7 @@ class MyApp extends StatelessWidget {
               if (jwt.length != 3) {
                 return LoginPage();
               } else {
-                var payload = json.decode(
-                    ascii.decode(base64.decode(base64.normalize(jwt[1]))));
+                var payload = json.decode(ascii.decode(base64.decode(base64.normalize(jwt[1]))));
                 return PageSelector(str, payload);
               }
             } else {
